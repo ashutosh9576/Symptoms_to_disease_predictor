5fdae8d2d0c722025b23efad89e0e3d09aaf0302
Ashutosh Jha <ashutoshjha@Ashutoshs-MacBook-Air.local>
2024-12-29 20:29:33 +0530

    updated_depriciated_code

--- symptom_project/urls.py
+++ symptom_project/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import include, re_path
 from django.contrib import admin
 from app import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$',views.index,name='index'),
-    url(r'^result',views.result,name='result'),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$',views.index,name='index'),
+    re_path(r'^result',views.result,name='result'),
 ]
